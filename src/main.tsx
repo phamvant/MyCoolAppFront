fd6c6c0cb8e45738280c5fd0037647235d62c82f
thuan <pham.t.286.01@gmail.com>
2025-04-19 01:20:09 +0900

    Remove unused Radix UI themes import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import AppRoutes from "./routes/AppRoutes.tsx";
 import { AuthProvider } from "./providers/AuthProvider";
 import { ThemeProvider } from "./providers/ThemeProvider";
-import "@radix-ui/themes/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
